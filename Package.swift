40df7c59604c11d6d0cf77c5c518afcbac2e5743
Bruno Coelho <4brunu@users.noreply.github.com>
2021-02-19 14:38:53 +0000

    Make Library dynamic

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "Kingfisher",
     platforms: [.iOS(.v10), .macOS(.v10_12), .tvOS(.v10), .watchOS(.v3)],
     products: [
-        .library(name: "Kingfisher", targets: ["Kingfisher"])
+        .library(name: "Kingfisher", type: .dynamic, targets: ["Kingfisher"])
     ],
     targets: [
         .target(
